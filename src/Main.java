3eebdad9251f1cf2638eff946b8b6d1edf8b8ae0
bhsingh0 <bhartisinghcse@gmail.com>
2025-10-05 00:04:06 +0100

    First push

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+/*
+ * @created 04/10/2025 -23:56
+ * @project Default (Template) Project
+ * @author  bhartisingh
+ *///TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!Updated");
+
+
+    }
+}
